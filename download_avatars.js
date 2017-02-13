66f257ee5974913d7315ac7ac92803b85a13aa98
GEDDE5 <geddes.3754@gmail.com>
2017-02-13 21:59:17 +0000

    Makes the request to GitHub for JSON data

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,40 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = 'GEDDE5';
 var GITHUB_TOKEN = '4f8e924b6235e2548083b44e2d1ac748349498fc';
+var USER_AGENT = GITHUB_USER;
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
+
+  let options = {
+    url: requestURL,
+    headers: { 'User-Agent': USER_AGENT }
+  }
+
+  request.get(options, function(err, response, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body));
+  });
 }
+
+
+// This doesn't work for some reason :()
+//   request.get(options)
+//     .on('error', function(err) {
+//       throw err;
+//     })
+//     .on('response', function(response) {
+//       console.log('Response status code:', response.statusCode);
+//     })
+//     .on('body', function(body) {
+//       console.log(body);
+//     });
+//     .pipe(fs.createWriteStream('response.json'));
 
 function cb(err, result) {
   console.log('Errors: ' + err);
